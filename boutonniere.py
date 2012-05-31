ace354ccda62a0086e23ceb4eb8a6df1318e0e2a
Valentine Svensson <valentine@nxn.se>
2012-05-31 15:23:43 +0200

    Implemented reference match counting and a function for counting reads in a fastq file

--- boutonniere.py
+++ boutonniere.py
@@ -2,13 +2,50 @@ from pybloomfilter import BloomFilter
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
 
-def create_reference_bloom_filter(fastq_file, capacity, error_rate, bf_file):
+def create_ref_bloom_filter(reference_fastq_file, capacity, error_rate, bf_file):
     """From a given FASTQ reference sequence creates a bloom filter file
     from each read.
     """
-    with open(fastq_file) as fastq_handle:
+    with open(reference_fastq_file) as fastq_handle:
         fastq_it = FastqGeneralIterator(fastq_handle)
         read_it = (read for _, read, _ in fastq_it)
         bf = BloomFilter(capacity, error_rate, bf_file)
         bf.update(read_it)
         bf.close()
+
+
+def total_reads(fastq_file):
+    """Get the total number of reads in a fastq file.
+    (Just reads all the fastq tuples of the file and counts.)
+    """
+    with open(fastq_file) as fastq_handle:
+        fastq_it = FastqGeneralIterator(fastq_handle)
+        for total_num_reads, _ in enumerate(fastq_it):
+            pass
+
+    return total_num_reads
+
+
+def count_matches(fastq_file, bf_file, ratio):
+    """Goes through a fastq file and checks a sample of reads if they
+    occur in the specified bloom filter.
+    """
+    bf = BloomFilter.open(bf_file)
+    fastq_handle = open(fastq_file)
+
+    fastq_it = FastqGeneralIterator(fastq_handle)
+    checked = 0
+    observed = 0
+    ratio = int(ratio)
+    for i, (_, read, _) in enumerate(fastq_it):
+        if i % ratio:
+            continue
+
+        checked += 1
+        if read in bf:
+            observed += 1
+
+    fastq_handle.close()
+    bf.close()
+
+    return checked, observed
